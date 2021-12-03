bf3a461e8ed3b3b78a4ea68bd3ff40b12c924d13
MathiasFabi <95499164+MathiasFabi@users.noreply.github.com>
2021-12-03 16:42:07 -0300

    Create Verifica_Letra.py

---
+++ Verifica_Letra.py
@@ -0,0 +1,9 @@
+# Para verificar se uma letra foi digitada
+def letra():
+  frase = input('Digite uma frase ')
+  if 'u' in frase:
+    print ('Vc digitou a letra u ')
+  else:
+    print('Vc não utilizou a letra u ')  
+
+letra()   
