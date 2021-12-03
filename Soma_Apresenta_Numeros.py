16e0215c9d61bd05b1ec866bb5494f228f1a9565
MathiasFabi <95499164+MathiasFabi@users.noreply.github.com>
2021-12-03 16:49:31 -0300

    Create Soma_Apresenta_Numeros.py

---
+++ Soma_Apresenta_Numeros.py
@@ -0,0 +1,9 @@
+# Soma e apresenta os numeros somados
+print('=' *40)
+print('{:^40}'.format('Soma de Valores'))
+print('=' *40)
+
+a = int(input('Digite um valor R$ '))
+b = int(input('Digite outro valor R$ '))
+c = a + b
+print('A soma entre {} e {} é igual a R$ {}'.format (a,b,c))
