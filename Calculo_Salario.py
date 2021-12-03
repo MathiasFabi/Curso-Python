96b1b1be8fe9cbd88e2c66949549af729fb97fe5
MathiasFabi <95499164+MathiasFabi@users.noreply.github.com>
2021-12-03 16:34:05 -0300

    Create Calculo_Salario.py

---
+++ Calculo_Salario.py
@@ -0,0 +1,6 @@
+# CalculoSalario
+funcionario = input('Digite o nome do funionário ')
+valorhora = float(input('Digite quanto o funcionário ganha por hora '))
+horastrab = float(input('Digite quantas horas o funcionário trabalhou no mês '))
+salario = valorhora * horastrab
+print('O salario do funcionario',funcionario,'é:', salario)
