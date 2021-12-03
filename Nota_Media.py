211a48f024aff64f30e62917de3eeea9751762d1
MathiasFabi <95499164+MathiasFabi@users.noreply.github.com>
2021-12-03 16:28:32 -0300

    Create Nota_Media.py

---
+++ Nota_Media.py
@@ -0,0 +1,9 @@
+# Curso-Python
+aluno = input('Digite o nome do aluno ')
+curso = input('Digite o curso ')
+nota1 = float(input('Digite a nota 1 : '))
+nota2 = float(input('Digite a nota 2 : '))
+nota3 = float(input('Digite a nota 3 : '))
+nota4 = float(input('Digite a nota 4 : '))
+notas = (nota1 + nota2 + nota3 + nota4) / 4
+print('O aluno', aluno, 'do curso de',curso, 'ficou com a média:',notas)
