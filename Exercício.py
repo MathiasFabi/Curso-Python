04eb5da78ba7a1e330fd600353401afb3a61397e
MathiasFabi <95499164+MathiasFabi@users.noreply.github.com>
2021-12-03 15:38:22 -0300

    Create Exercício.py

---
+++ Exercício.py
@@ -0,0 +1,6 @@
+nome = input('Digite o seu nome ')
+idade = int(input('Digite a sua idade '))
+idade2 = idade + 10
+print('Seu nome é:',  nome)
+print('Sua idade é:', idade)
+print('Daqui a 10 anos você terá:', idade2)
